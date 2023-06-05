cba04d3b6bf886b6eab1c96a9eee61a927dea3ac
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-06-05 19:45:09 +0700

    [feature] store tx and test on db package

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,15 +15,17 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	var err error
 
-	conn, err := sql.Open(dbDriver, dbSources)
+	testDB, err = sql.Open(dbDriver, dbSources)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
